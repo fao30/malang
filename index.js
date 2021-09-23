ad64b11850938fc794823e7b8e52d9c2ac38ea4c
Fakhrul Arifin <fakhrularifin@Fakhruls-MacBook-Pro.local>
2021-09-23 19:53:04 +0700

    revisi ke 2 jam 19:53

--- index.js
+++ index.js
@@ -1,20 +1,99 @@
 let dataMe = [];
 //untuk masuk ke ke login
 function loginPage(login, pass) {
-  let dataLogin = [
-    ["admin1", 123, "admin"],
-    ["admin2", 456, "admin"],
-    ["me1", 123, "me"],
-    ["me2", 456, "me"],
-    ["me3", 789, "me"],
+  let data = [
+    {
+      nama: "jaka",
+      alamat: "bekasi",
+      telp: 081212,
+      due: 01 / 10 / 2021,
+      nominal: 500000,
+    },
+    {
+      nama: "jaki",
+      alamat: "jakarta",
+      telp: 081212,
+      due: 10 / 10 / 2021,
+      nominal: 600000,
+    },
+    {
+      nama: "jaku",
+      alamat: "solo",
+      telp: 081212,
+      due: 15 / 10 / 2021,
+      nominal: 700000,
+    },
+    {
+      nama: "jake",
+      alamat: "semarang",
+      telp: 081212,
+      due: 25 / 09 / 2021,
+      nominal: 800000,
+    },
+    {
+      nama: "jako",
+      alamat: "jogja",
+      telp: 081212,
+      due: 26 / 09 / 2021,
+      nominal: 900000,
+    },
+    {
+      nama: "kaka",
+      alamat: "makasar",
+      telp: 081212,
+      due: 27 / 09 / 2021,
+      nominal: 1000000,
+    },
+    {
+      nama: "kaki",
+      alamat: "pontianak",
+      telp: 081212,
+      due: 01 / 09 / 2021,
+      nominal: 1100000,
+    },
+    {
+      nama: "kaku",
+      alamat: "fakfak",
+      telp: 081212,
+      due: 02 / 09 / 2021,
+      nominal: 1200000,
+    },
+    {
+      nama: "kake",
+      alamat: "jayapura",
+      telp: 081212,
+      due: 15 / 09 / 2021,
+      nominal: 1300000,
+    },
+    {
+      nama: "kako",
+      alamat: "aceh",
+      telp: 081212,
+      due: 23 / 09 / 2021,
+      nominal: 1400000,
+    },
+  ];
+  let users = [
+    {
+      id: "admin",
+      password: "123",
+      role: "admin",
+    },
+    {
+      id: "me",
+      password: "123",
+      role: "user",
+    },
   ];
   let result = [];
-  for (let i = 0; i < dataLogin.length; i++) {
-    if (dataLogin[i][0] == login && dataLogin[i][1] == pass) {
-      result.push(true, dataLogin[i][2]);
+  for (let i = 0; i < users.length; i++) {
+    if (users[i]["id"] == login && users[i]["password"] == pass) {
+      result.push(true, users[i]["role"]);
+      // console.log(result);
       break;
     }
-    if (i == dataLogin.length - 1) {
+    if (i == users.length - 1) {
+      Swal.fire("ERROR!", "maaf password / id anda salah, fokus bos!", "error");
       return "error";
     }
   }
@@ -24,13 +103,14 @@ function loginPage(login, pass) {
 let id = document.querySelector("#id");
 let pass = document.querySelector("#pass");
 let loginForm = document.querySelector("#loginForm");
+let hasil = "";
 loginForm.addEventListener("submit", function (event) {
   event.preventDefault();
   let hasilLogin = loginPage(id.value, pass.value);
-  console.log(hasilLogin);
+  hasil = hasilLogin;
   if (hasilLogin != "error") {
     document.querySelector("#loginForm").style.display = "none";
-    document.querySelector(".testButton").style.display = "flex";
+    document.querySelector(".beranda").style.display = "flex";
   }
   id.value = "";
   pass.value = "";
@@ -38,22 +118,32 @@ loginForm.addEventListener("submit", function (event) {
 // home button untuk pilih buron apa pinjaman
 let pinjam = document.querySelector("#pinjamButton");
 pinjam.addEventListener("click", function () {
-  document.querySelector(".testButton").style.display = "none";
-  document.querySelector("#pinjaman").style.display = "flex";
+  if (hasil[1] == "admin") {
+    document.querySelector(".beranda").style.display = "none";
+    document.querySelector("#pinjaman").style.display = "flex";
+  } else {
+    document.querySelector(".beranda").style.display = "none";
+    document.querySelector("#pinjaman2").style.display = "flex";
+  }
 });
 let buron = document.querySelector("#buronButton");
 buron.addEventListener("click", function () {
-  document.querySelector(".testButton").style.display = "none";
+  document.querySelector(".beranda").style.display = "none";
   document.querySelector("#buron").style.display = "flex";
 });
 // //
 let pinjHome = document.querySelector("#pinjamanHome");
 pinjHome.addEventListener("click", function () {
   document.querySelector("#pinjaman").style.display = "none";
-  document.querySelector(".testButton").style.display = "flex";
+  document.querySelector(".beranda").style.display = "flex";
+});
+let pinjHome2 = document.querySelector("#pinjamanHome2");
+pinjHome2.addEventListener("click", function () {
+  document.querySelector("#pinjaman2").style.display = "none";
+  document.querySelector(".beranda").style.display = "flex";
 });
 let buronHome = document.querySelector("#buronHome");
 buronHome.addEventListener("click", function () {
   document.querySelector("#buron").style.display = "none";
-  document.querySelector(".testButton").style.display = "flex";
+  document.querySelector(".beranda").style.display = "flex";
 });
