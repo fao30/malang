3e65545a8ed28b93f085360f03825b93aabb596b
Fakhrul Arifin <fakhrularifin@Fakhruls-MacBook-Pro.local>
2021-09-23 22:13:00 +0700

    masuk loop html

--- index.js
+++ index.js
@@ -147,3 +147,96 @@ buronHome.addEventListener("click", function () {
   document.querySelector("#buron").style.display = "none";
   document.querySelector(".beranda").style.display = "flex";
 });
+
+// const todoList = document.querySelector(".todo-list");
+
+// function render() {
+//   let dataAll = [
+//     {
+//       nama: "jaka",
+//       alamat: "bekasi",
+//       telp: 089485736432,
+//       dueDate: "2021-09-07",
+//       nominal: 500000,
+//     },
+//     {
+//       nama: "jaki",
+//       alamat: "jakarta",
+//       telp: 084324243534,
+//       dueDate: "2021-11-01",
+//       nominal: 600000,
+//     },
+//     {
+//       nama: "jaku",
+//       alamat: "solo",
+//       telp: 084324243534,
+//       dueDate: "2021-09-30",
+//       nominal: 700000,
+//     },
+//     {
+//       nama: "jake",
+//       alamat: "semarang",
+//       telp: 084324243534,
+//       dueDate: "2021-09-20",
+//       nominal: 800000,
+//     },
+//     {
+//       nama: "jako",
+//       alamat: "jogja",
+//       telp: 084324243534,
+//       dueDate: "2021-08-01",
+//       nominal: 900000,
+//     },
+//     {
+//       nama: "kaka",
+//       alamat: "makasar",
+//       telp: 084324243534,
+//       dueDate: "2021-09-26",
+//       nominal: 1000000,
+//     },
+//     {
+//       nama: "kaki",
+//       alamat: "pontianak",
+//       telp: 084324243534,
+//       dueDate: "2021-09-24",
+//       nominal: 1100000,
+//     },
+//     {
+//       nama: "kaku",
+//       alamat: "fakfak",
+//       telp: 084324243534,
+//       dueDate: "2021-09-05",
+//       nominal: 1200000,
+//     },
+//     {
+//       nama: "kake",
+//       alamat: "jayapura",
+//       telp: 084324243534,
+//       dueDate: "2021-10-01",
+//       nominal: 1300000,
+//     },
+//     {
+//       nama: "kako",
+//       alamat: "aceh",
+//       telp: 084324243534,
+//       dueDate: "2021-09-01",
+//       nominal: 1400000,
+//     },
+//   ];
+//   // put all task to html
+//   for (let i = 0; i < dataAll.length; i++) {
+//     // create div
+//     const todo = document.createElement("div");
+//     todo.classList.add("todo");
+//     // create list
+//     const newTodo = document.createElement("li");
+//     newTodo.innerText = `${dataAll[i].nama} -- ${dataAll[i].alamat} -- ${dataAll[i].telp} -- ${dataAll[i].dueDate} -- ${dataAll[i].nominal}`;
+//     newTodo.classList.add("todo-item");
+//     todo.appendChild(newTodo);
+//     // todo.appendChild(button);
+
+//     // append to todoList
+//     todoList.appendChild(todo);
+//   }
+// }
+// render();
